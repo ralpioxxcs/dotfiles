2359dec10da29ffb575fd5f88aab67121992a352
ralpioxxcs <tmfmfkal1123@naver.com>
2022-09-23 21:24:01 +0900

    bug fix & add vsc

--- config.sh
+++ config.sh
@@ -419,9 +419,9 @@ install_lazygit() {
     echo "install lazygit.."
     custom_install_wrapper \
     'export LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '\''"tag_name": "v\K[0-35.]+'\'')' \
-    'curl -Lo --silent lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"' \
+    'curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"' \
     'sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit' \
-    >/dev/null 2>&1 &
+    dev/null 2>&1 &
     spinner
   fi
   echo -e "${COLOR_GREEN}${pac} has successfully installed!${COLOR_NONE}"
